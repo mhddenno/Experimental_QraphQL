e0cabafec0b11ade19f6fc9263dc4486383caccb
Mohamad Denno <dennokhmhd@gmail.com>
2022-05-27 08:45:14 +0200

    sending params c to v

--- app/controllers/structs_controller.rb
+++ app/controllers/structs_controller.rb
@@ -1,4 +1,10 @@
 class StructsController < ApplicationController
   def index
+    @magic = play(10000)
+  end
+
+  def play(times)
+    block = proc{|n| 'Mo ist Toll' * n}
+    return block.call(times)
   end
 end
